28559ea515433d3fad0f09a2dd0c3c7b3b0d0085
eliasollila <elias.ollila@syk.fi>
2022-10-07 09:03:24 +0300

    uji

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(int row=6; row>0; row--)
+        {
+            for(int col=row; col>0; col--)
+            {
+                String sym="*";
+                System.out.print(sym);
+            }
+            System.out.println();
+        }
+
+        int rounds=6;
+        for(int row=1; row<=rounds; row++)
+        {
+            for(int star=row; star>0; star--)
+            {
+                System.out.print("*");
+            }
+            for(int dash=rounds-row; dash>0; dash--)
+            {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
     }
 }
